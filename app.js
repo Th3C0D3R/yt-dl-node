684f17552451ce7c51515141fb925bbf00e59ded
TH3C0D3R <7798500+Th3C0D3R@users.noreply.github.com>
2025-09-25 23:08:13 +0200

    update

--- app.js
+++ app.js
@@ -17,10 +17,15 @@ const COOKIES_FILE = process.env.COOKIES_FILE;
 const PORT = process.env.PORT;
 const ISDEBUG = process.env.DEBUG === 'true';
 
-const bestFormat = "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b";
-var isDownloading = false;
+const STATUS = {
+    QUEUED: 'queued',
+    DOWNLOADING: 'downloading',
+    ERROR: 'error'
+}
 
-isDownloading = ISDEBUG;
+const bestFormat = "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b";
+var isDownloading = ISDEBUG;
+var currentItem = null;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -28,15 +33,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-let clients = [];
+let progressClients = [];
+let queueClients = [];
 let queue = readQueue();
 let errored = [];
 
 if (queue.length > 0) {
     for (; queue.length;) {
         try {
-            const { url, format } = queue.shift();
-            await download(url, format);
+            const { url, format, info } = queue.shift();
+            await download(url, format, info);
         } catch (error) {
             console.error(error);
             errored.push({ url, format });
@@ -50,15 +56,34 @@ app.get('/progress', (req, res) => {
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
     res.flushHeaders();
-    clients.push(res);
+    progressClients.push(res);
 
     req.on('close', () => {
-        clients = clients.filter(c => c !== res);
+        progressClients = progressClients.filter(c => c !== res);
+    });
+});
+
+// SSE route for queue updates
+app.get('/queue', (req, res) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.flushHeaders();
+    queueClients.push(res);
+    if (queue.length > 0)
+        res.write(`data: ${JSON.stringify(queue.map(item => ({ title: item.info.title || 'Unknown_Title', id: item.info.id, currentStatus: currentItem == item.info.id ? STATUS.DOWNLOADING : STATUS.QUEUED })))}\n\n`);
+    else
+        res.write(`data: ${JSON.stringify([])}\n\n`);
+    req.on('close', () => {
+        queueClients = queueClients.filter(c => c !== res);
     });
 });
 
 function sendProgress(data) {
-    clients.forEach(client => client.write(`data: ${JSON.stringify(data)}\n\n`));
+    progressClients.forEach(client => client.write(`data: ${JSON.stringify(data)}\n\n`));
+}
+function sendQueueUpdate() {
+    queueClients.forEach(client => client.write(`data: ${JSON.stringify(queue.map(item => ({ title: item.info.title || 'Unknown_Title', id: item.info.id, currentStatus: currentItem == item.info.id ? STATUS.DOWNLOADING : STATUS.QUEUED })))}\n\n`));
 }
 
 app.get('/', (req, res) => {
@@ -76,13 +101,26 @@ app.post('/download', async (req, res) => {
         return;
     }
     try {
+        currentItem = queue.find(item => item.url === url);
         var ret = await download(url, format, info);
         res.json(ret);
 
     } catch (error) {
         console.error(error);
+        
         res.status(500).json({ error: 'Download failed' });
     }
+});
+
+app.post('/remove', async (req, res) => {
+    const { id } = req.body;
+    if (!id) return res.status(400).json({ error: 'No ID provided' }); 
+
+    // Remove the item from the queue
+    queue = queue.filter(item => item.id !== id);
+    saveQueue();
+    sendQueueUpdate();
+    res.json({ success: true });
 });
 
 async function download(url, format, info) {
@@ -105,10 +143,9 @@ async function download(url, format, info) {
     //console.log(options.ffmpegLocation);
     //console.log(options.format);
 
-    /* const process = youtubedl.exec(url, options);
+    const process = youtubedl.exec(url, options);
 
     process.stdout.on('data', (chunk) => {
-        //console.log(chunk.toString());
         const msg = chunk.toString();
         const match = msg.match(/(\d+\.\d)%/);
         if (match) {
@@ -123,8 +160,12 @@ async function download(url, format, info) {
 
     process.on('close', () => {
         sendProgress({ percent: 100, title: info.title, done: true });
+        currentItem = null;
         isDownloading = false;
-    }); */
+        queue = queue.filter(item => item.url !== url);
+        saveQueue();
+        sendQueueUpdate();
+    });
 
     return { success: true }
 
@@ -145,7 +186,7 @@ function readQueue() {
 }
 
 function saveQueue() {
-    var queueData = queue.map(item => `${item.url}#${item.format}#${info.title || 'Unknown_Title'}#${info.uploader || 'Unknown_Channel'}`).join(";");
+    var queueData = queue.map(item => `${item.url}#${item.format}#${item.info.title || 'Unknown_Title'}#${item.info.uploader || 'Unknown_Channel'}`).join(";");
     fs.writeFileSync(path.join(__dirname, ".queue"), queueData, { encoding: "utf-8", flag: "w" });
 }
 
